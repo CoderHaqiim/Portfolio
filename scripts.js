21b5d035a2efb53643eab60dcb6fb386b753ee6e
Haqiim <hqcoder.jh@gmail.com>
2024-01-06 12:09:33 +0100

    The first commit and main branch of my portfolio website

---
+++ scripts.js
@@ -0,0 +1,153 @@
+const aboutPage = document.querySelector("#about-page")
+const exit = document.querySelector("#exit-button")
+const shadow = document.querySelector("#shadow")
+const aboutBtn= document.querySelector(".about-btn")
+const resumeBtn = document.querySelector(".resume-btn")
+const nav = document.querySelector("#nav")
+const logo = document.querySelector("#logo")
+const homeBtn = document.querySelector("#home-btn")
+const projectsBtn = document.querySelector("#projects-btn")
+const abt = document.querySelector("#abt")
+const topbar = document.querySelector("#topbar")
+const midbar = document.querySelector("#midbar")
+const botbar = document.querySelector("#botbar")
+const hamburger = document.querySelector("#hamburger")
+const optbar = document.querySelector("#optbar")
+const tech = document.querySelector("#technologies")
+const techUsed = document.querySelector("#tech-used")
+const imgContainer = document.querySelectorAll(".img-container")
+let count = 0
+
+
+console.log(innerWidth)
+aboutBtn.onclick = () =>{
+    shadow.style.display = "flex"
+}
+abt.onclick = () =>{
+    shadow.style.display = "flex"
+}
+exit.onclick = () =>{
+    shadow.style.display = "none"
+}
+
+const createItemsForId1 = (container) =>{
+    const techsUsed = ['javascript','css','html']
+    for (item of techsUsed){
+        techUsed.innerHTML +=`<p>${item}</p>`
+    }
+    drag(container)
+}
+
+const drag = (container) =>{
+    const rect = container.getBoundingClientRect()
+    onmousemove = (e) =>{
+        const x = e.clientX
+        const y = e.clientY
+
+        tech.style.top =`${y - rect.top-150}px`;
+        tech.style.left =`${x - rect.left-55}px`;
+    }
+}
+
+const createItemsForId2 = (container) =>{
+    const techsUsed = ['react','tailwind','swiper']
+    for (item of techsUsed){
+        techUsed.innerHTML +=`<p>${item}</p>`
+    }
+    drag(container)
+}
+const createItemsForId3 = (container) =>{
+    const techsUsed = ['react','javascript','css','html']
+    for (item of techsUsed){
+        techUsed.innerHTML +=`<p>${item}</p>`
+    }
+    drag(container)
+}
+const createItemsForId4 = (container) =>{
+    const techsUsed = ['react','tailwind','swiper']
+    for (item of techsUsed){
+        techUsed.innerHTML +=`<p>${item}</p>`
+    }
+    drag(container)
+}
+
+
+imgContainer.forEach((container)=>{
+    container.onmouseover=(e) => {
+        e.target.id == "img1" && createItemsForId1(container)
+        e.target.id == "img2" && createItemsForId2(container)
+        e.target.id == "img3" && createItemsForId3(container)
+        e.target.id == "img4" && createItemsForId4(container)
+
+        container.append(tech)
+        tech.style.display = "flex"
+    }
+    container.onmouseout = () =>{
+        tech.style.display = "none"
+        techUsed.innerHTML = " ";
+    }
+})
+
+const changeNavColor = () =>{
+    nav.style.backgroundColor = "var(--foot-color)"
+    resumeBtn.style.color = "var(--foot-color)"
+}
+const navBorderChange = () =>{
+    projectsBtn.style.borderBottom = "3px solid var(--action)"
+    homeBtn.style.borderBottom = "none"
+}
+const normalNav = () =>{
+    homeBtn.style.borderBottom = "3px solid var(--action)"
+    projectsBtn.style.borderBottom = "none"
+}
+const navTransparent = () =>{
+    nav.style.backgroundColor = "transparent"
+    resumeBtn.style.color = "var(--purple)"
+}
+onscroll = () =>{
+    let y = scrollY
+    let x = scrollX
+    y >= 80? changeNavColor(): navTransparent()
+    y >= 680? navBorderChange() : normalNav()
+}
+onload = () =>{
+    homeBtn.classList.add("clicked-nav")
+}
+
+
+
+//copied library
+
+   function translate(obj,coordinate,distance){
+    obj.style.transform = `translate${coordinate}(${distance}px)`;
+   }
+    
+   function rotate(obj,coordinate,angle){
+       obj.style.transform = `rotate${coordinate}(${angle}deg)`;
+   }
+    
+   function appear(obj,orientation){
+       obj.style.display = orientation;
+   };
+    
+   function click(obj,x){
+       obj.addEventListener("click",x)
+   }
+
+   click(hamburger,()=>{
+    if(count === 0){
+    appear(midbar,"none");
+    topbar.style.transform = `translateY(8.7px) rotateZ(-135deg)`;
+    botbar.style.transform = `translateY(-8.7px) rotateZ(135deg)`;
+    appear(optbar,"block")
+    optbar.style.animationName = "down-slide";
+    count = 1;
+    }else{
+        appear(midbar,"block");
+        topbar.style.transform = `translateY(0px) rotateZ(0deg)`;
+        botbar.style.transform = `translateY(0px) rotateZ(0deg)`;
+        optbar.style.animationName = "up-slide";
+        setTimeout(()=>{appear(optbar,"none")},150)
+        count = 0;
+    }
+})
